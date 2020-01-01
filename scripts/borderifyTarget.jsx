fc4c856466172ca4acd36919af7465d48bf3c088
Nicholas de Paola <ndepaola97@gmail.com>
2020-01-01 15:04:01 +1000

    improvements to borderify (requires updated template)

--- scripts/borderifyTarget.jsx
+++ scripts/borderifyTarget.jsx
@@ -9,6 +9,6 @@ $.evalFile(filePath + "/scripts/borderify.jsx");
 // Ensure the file can be borderify'd, then do it
 if (file[0]) {
   if (file.constructor != Folder) {
-    borderify(file);
+    borderify(file, 0);
   }
 }
